ac681f7378f3c6720f8c959dea3eb52ef0e04d0b
marawan gamal <mgamal@marawans-MacBook-Pro.local>
2020-02-11 21:34:15 -0500

    driver

--- driver.py
+++ driver.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
 
     # IMPORT IMAGE
 
-    img = cv2.imread("2.jpg")
-    img_CIElab = cv2.cvtColor(np.copy(img), cv2.COLOR_BGR2LAB)
+    img = io.imread("imgs/bear.jpg",plugin='matplotlib')
+    img_CIElab = color.rgb2lab(rgb2)
 
 
     # KMEANS EXAMPLE
@@ -21,10 +21,7 @@ if __name__ == '__main__':
 
     # SLIC EXAMPLE
 
-    rgb2 = io.imread("2.jpg",plugin='matplotlib')
-    img_CIElab2 = color.rgb2lab(rgb2)
-
     sl = SLIC()
-    mus, sets, = sl.get_superpixels(img_CIElab2, k=100, iters=5)
+    mus, sets, = sl.get_superpixels(img_CIElab, k=100, iters=5)
     clr_mask = sl.get_color_mask(mus, sets)
     plt.imshow(clr_mask); plt.show()
